7b936155c56f067aa41495123e4666a1d6cb0cae
hossam <hossam@hossam.com>
2021-12-28 13:10:53 +0200

    5 ** 28-12-2021 ** 1:00 PM - work -good

--- home/hmuwy1m8l95t/project/polls/urls.py
+++ home/hmuwy1m8l95t/project/polls/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('page2', views.page2)
-    # path('')
 ]
+
+
--- home/hmuwy1m8l95t/project/polls/views.py
+++ home/hmuwy1m8l95t/project/polls/views.py
@@ -1,10 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     html_file = 'polls/index.html'
-    # return HttpResponse("Hello, world. You're at the polls index. ya hossam 22 33 ")
     return render(request, html_file)
 
 
