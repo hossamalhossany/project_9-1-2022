b9d0bc32cf75f1b4282969343abe60de9bdf9999
hossam <hossam@hossam.com>
2022-01-02 16:24:22 +0200

    15 ** 2-1-2022 ** 2:07 PM - work -- too good

--- home/hmuwy1m8l95t/project/polls/views.py
+++ home/hmuwy1m8l95t/project/polls/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from rest_framework import routers
 
 
 def index(request):
--- home/hmuwy1m8l95t/project/project/settings.py
+++ home/hmuwy1m8l95t/project/project/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls',
 ]
 
 MIDDLEWARE = [
@@ -114,12 +113,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = '/static/'
-#
-# MEDIA_URL = '/media/'
-# STATICFILES_DIRS = [BASE_DIR, 'static']
-# MEDIA_ROOT = 'home/hmuwy1m8l95t/project/media'
-# STATIC_ROOT = 'home/hmuwy1m8l95t/project/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
